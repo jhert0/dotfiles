a1f1f1314578cf5587b475ce1d53c47274e94872
endoffile78 <endoffile78@yahoo.com>
2017-08-20 12:54:14 -0500

    Change colors for git prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -12,9 +12,9 @@ virtualenv_info(){
 set_prompt(){
 	PS1="%B%F{blue}%n%b%f@%m %B%1~%b"
 	if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
-		PS1+=" %{$fg_bold[blue]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
+		PS1+=" %{$fg_bold[magenta]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
 		if [ $(git status --short | wc -l) -gt 0 ]; then
-			PS1+=" %{$fg[red]%}+$(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
+			PS1+=" %{$fg_bold[green]%}+$(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
 		fi
 	fi
 	PS1+=" %% "
