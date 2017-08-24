4de8f4ca101d95c660a11a9bf52767a7ab112aa9
endoffile78 <endoffile78@yahoo.com>
2017-08-24 18:49:18 -0500

    Update setup-links.sh and setup-dirs.sh

--- setup-dirs.sh
+++ setup-dirs.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-mkdir -p ~/bin
-mkdir -p ~/media
-mkdir -p ~/data
-mkdir -p ~/src
-mkdir -p ~/docs
-mkdir -p ~/tmp
-mkdir -p ~/vms
+# this is for creating the directories i use on
+# a computer with one hard drive
+
+dirs=("bin" "media" "data" "src" "docs" "tmp" "vms")
+
+for file in "${dirs[@]}"; do
+	echo "Creating directory ${HOME}/${file}"
+	mkdir -p "${HOME}/${file}"
+done
--- setup-links.sh
+++ setup-links.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+# I use this for computers with multiple harddrives
+
 MOUNTPOINT="/mnt/hdd"
-dirs=("media" "data" "docs")
+dirs=("media" "data" "docs" "vms" "tmp")
 
 for file in "${dirs[@]}"; do
-	echo "Creating symlink ${HOME}/${file}"
 	mkdir -p "${MOUNTPOINT}/${file}"
+	echo "Creating symlink ${HOME}/${file}"
 	ln -sf "${MOUNTPOINT}/${file}" "${HOME}/"
 done
