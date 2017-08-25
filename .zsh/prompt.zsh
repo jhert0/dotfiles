7514ca0760bfd28a4616107f49319438c14249af
endoffile78 <endoffile78@yahoo.com>
2017-08-24 22:11:42 -0500

    Update PROMPT and SPROMPT

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -13,8 +13,17 @@ set_prompt(){
 	PS1="%B%F{blue}%n%b%f@%m %B%1~%b"
 	if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
 		PS1+=" %{$fg_bold[magenta]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
-		if [ $(git status --short | wc -l) -gt 0 ]; then
-			PS1+=" %{$fg_bold[green]%}+$(git status --short | wc -l | awk '{$1=$1};1')%{$reset_color%}"
+		modified=$(git status --short | grep " M " | wc -l)
+		if [ $modified  -gt 0 ]; then
+			PS1+=" %{$fg_bold[green]%}~$modified%{$reset_color%}"
+		fi
+		untracked=$(git status --short | grep "??" | wc -l)
+		if [ $untracked  -gt 0 ]; then
+			PS1+=" %{$fg_bold[yellow]%}+$untracked%{$reset_color%}"
+		fi
+		deleted=$(git status --short | grep " D " | wc -l)
+		if [ $deleted -gt 0 ]; then
+			PS1+=" %{$fg_bold[red]%}-$deleted%{$reset_color%}"
 		fi
 	fi
 	PS1+=" %% "
@@ -23,5 +32,5 @@ set_prompt(){
 typeset -a precmd_functions
 precmd_functions+=set_prompt
 
-SPROMPT="zsh: correct %F{red}%R%f to %F{green}%r%f [nyae]? "
+SPROMPT="zsh: correct %{$fg_bold[red]%}%R%f%{$reset_color%} to %{$fg_bold[green]%}%r%f%{$reset_color%} [nyae]? "
 RPROMPT="%(?..%{$fg_bold[red]%}%?%{$reset_color%})"
