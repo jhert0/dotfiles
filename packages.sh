03a3215d7315656286294f508a1bcfc7e8081270
endoffile78 <endoffile78@yahoo.com>
2017-11-11 19:39:47 -0600

    Add some more packages

--- packages.sh
+++ packages.sh
@@ -2,4 +2,5 @@
 
 # Packages I commonly install
 
-sudo pacman -Syu --needed aspell aspell-en vlc mpv rxvt-unicode emacs xorg cinnamon weechat xclip mu libreoffice evince gnome-keyring mplayer nitrogen
+sudo pacman -Syu --needed aspell aspell-en vlc mpv rxvt-unicode emacs xorg cinnamon \
+	 weechat xclip mu libreoffice evince gnome-keyring mplayer nitrogen whois steam
