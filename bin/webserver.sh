d528998f3d07341e589a57eaa0b69117450953d7
endoffile78 <endoffile78@yahoo.com>
2018-09-02 12:37:35 -0500

    Start and stop mariadb too

--- bin/webserver.sh
+++ bin/webserver.sh
@@ -6,10 +6,12 @@ case $action in
 	"on")
 		sudo systemctl start nginx.service
 		sudo systemctl start php-fpm.service
+		sudo systemctl start mariadb.service
 		;;
 	"off")
 		sudo systemctl stop nginx.service
 		sudo systemctl stop php-fpm.service
+		sudo systemctl stop mariadb.service
 		;;
 	*)
 		echo "Unknown action."
