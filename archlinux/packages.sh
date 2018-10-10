d46871134ad250c432d5f9897f286173dfaaaee0
endoffile78 <endoffile78@yahoo.com>
2018-10-09 21:41:16 -0500

    Add some more packages

--- archlinux/packages.sh
+++ archlinux/packages.sh
@@ -3,4 +3,4 @@
 sudo pacman -Syu --needed vlc rxvt-unicode cinnamon \
      weechat xclip tmux libreoffice evince steam firefox \
      gnome-keyring emacs mpd ncmpcpp git openssh gnupg \
-     zsh
+     zsh xorg lightdm lightdm-gtk-greeter
