ea123af23ad2e9501144932e66a555c0ac5df110
endoffile78 <endoffile78@yahoo.com>
2019-02-05 15:58:20 -0600

    Update dotfiles

--- .zsh/completion.zsh
+++ .zsh/completion.zsh
@@ -4,8 +4,13 @@ setopt completeinword
 
 # bind UP and DOWN arrow keys
 zmodload zsh/terminfo
-bindkey "$terminfo[kcuu1]" history-substring-search-up
-bindkey "$terminfo[kcud1]" history-substring-search-down
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+    bindkey "$terminfo[kcuu1]" history-substring-search-up
+fi
+
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+    bindkey "$terminfo[kcud1]" history-substring-search-down
+fi
 
 # bind UP and DOWN arrow keys (compatibility fallback
 # for Ubuntu 12.04, Fedora 21, and MacOSX 10.9 users)
