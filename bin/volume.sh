fdd528ed8cac00ebc1bb44c5d034a4aaf706ac38
endoffile78 <endoffile78@yahoo.com>
2019-07-15 22:24:30 -0500

    Update dunst config and use dunstify

--- bin/volume.sh
+++ bin/volume.sh
@@ -6,14 +6,14 @@ num=$2
 case "$action" in
 	"inc")
 		amixer set Master "$num%+"
-		notify-send "Volume" "The volume is now $(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')"
+		dunstify "Volume" "The volume is now $(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')"
 		;;
 	"dec")
 		amixer set Master "$num%-"
-		notify-send "Volume" "The volume is now $(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')"
+		dunstify "Volume" "The volume is now $(amixer sget Master | grep 'Left:' | awk -F'[][]' '{ print $2 }')"
 		;;
 	"toggle")
 		amixer set Master toggle
-		notify-send "Volume" "The volume is now muted."
+		dunstify "Volume" "The volume is now muted."
 		;;
 esac
