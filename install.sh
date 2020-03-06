4d632b2d76872f3cad35bc3a34a1ad1665539e75
endoffile78 <endoffile78@yahoo.com>
2020-03-05 19:12:31 -0600

    Change font in alacritty and change default terminal in tmux

--- install.sh
+++ install.sh
@@ -6,68 +6,68 @@ PWD=$(pwd)
 backup="$HOME/old_dotfiles"
 
 configure_zsh(){
-	echo "Configuring zsh..."
+    echo "Configuring zsh..."
 
-	if [ "$SHELL" != "/bin/zsh" ]; then
-		chsh -s "$(which zsh)"
-	fi
+    if [ "$SHELL" != $(which zsh) ]; then
+        chsh -s "$(which zsh)"
+    fi
 
-	if [ ! -d "$HOME/.zplug" ]; then
-		echo "Installing zplug..."
-		git clone https://github.com/zplug/zplug "$HOME/.zplug"
-	fi
+    if [ ! -d "$HOME/.zplug" ]; then
+        echo "Installing zplug..."
+        git clone https://github.com/zplug/zplug "$HOME/.zplug"
+    fi
 }
 
 configure_tmux(){
     plugins_dir="$HOME/.tmux/plugins"
-	echo "Configuring tmux..."
-	if [ ! -d "$plugins_dir" ]; then
-		echo "Installing plugins..."
-		mkdir -p "$plugins_dir"
-		git clone https://github.com/tmux-plugins/tpm "$plugins_dir/tpm"
-		git clone https://github.com/tmux-plugins/tmux-battery "$plugins_dir/tmux-battery"
-	fi
+    echo "Configuring tmux..."
+    if [ ! -d "$plugins_dir" ]; then
+        echo "Installing plugins..."
+        mkdir -p "$plugins_dir"
+        git clone https://github.com/tmux-plugins/tpm "$plugins_dir/tpm"
+        ~/.tmux/plugins/tpm/bin/install_plugins
+    fi
 }
 
 configure_emacs(){
-	echo "Configuring emacs..."
-	if [ ! -d "$HOME/.emacs.d" ]; then
-		echo "Cloning emacs configuration..."
-		git clone --recursive https://github.com/endoffile78/dotemacs "$HOME/.emacs.d"
-	fi
+    echo "Configuring emacs..."
+    if [ ! -d "$HOME/.emacs.d" ]; then
+        echo "Cloning emacs configuration..."
+        git clone --recursive https://github.com/endoffile78/dotemacs "$HOME/.emacs.d"
+    fi
 }
 
 configure_nvim(){
-	echo "Configuring neovim..."
-	if [[ ! -d "$HOME/.config/nvim" ]]; then
-		git clone https://github.com/endoffile78/dotneovim ~/.config/nvim/
-		curl -fLo ~/.config/nvim/autoload/plug.vim --create-dir https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-	fi
+    echo "Configuring neovim..."
+    if [[ ! -d "$HOME/.config/nvim" ]]; then
+        git clone https://github.com/endoffile78/dotneovim ~/.config/nvim/
+        curl -fLo ~/.config/nvim/autoload/plug.vim --create-dir https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    fi
 }
 
 configure_vim(){
     echo "Configuring vim..."
-	if [[ ! -d "$HOME/.vim" && -d "$HOME/.vimrc" ]]; then
+    if [[ ! -d "$HOME/.vim" && -d "$HOME/.vimrc" ]]; then
         ln -s ~/.config/nvim/ ~/.vim
         ln -s ~/.config/nvim/init.vim ~/.vimrc
-	fi
+    fi
 }
 
 backup(){
-	mkdir -p "$backup"
-	mkdir -p "$backup/.config"
-	for file in "${dotfiles[@]}"; do
-		if [[ -f "$HOME/$file" || -d "$HOME/$file" ]]; then
-			echo "Backing up ${HOME}/${file}"
-			mv "$HOME/$file" "$backup/$file"
-		fi
-	done
-	for file in "${configs[@]}"; do
-		if [[ -f "$HOME/.config/$file" || -d "$HOME/.config/$file" ]]; then
-			echo "Backing up ${HOME}/.config/${file}"
-			mv "$HOME/.config/$file" "$backup/.config/$file"
-		fi
-	done
+    mkdir -p "$backup"
+    mkdir -p "$backup/.config"
+    for file in "${dotfiles[@]}"; do
+        if [[ -f "$HOME/$file" || -d "$HOME/$file" ]]; then
+            echo "Backing up ${HOME}/${file}"
+            mv "$HOME/$file" "$backup/$file"
+        fi
+    done
+    for file in "${configs[@]}"; do
+        if [[ -f "$HOME/.config/$file" || -d "$HOME/.config/$file" ]]; then
+            echo "Backing up ${HOME}/.config/${file}"
+            mv "$HOME/.config/$file" "$backup/.config/$file"
+        fi
+    done
 }
 
 backup
@@ -78,11 +78,11 @@ configure_nvim
 configure_vim
 
 for file in "${dotfiles[@]}"; do
-	echo "Creating symlink ${HOME}/${file}"
-	ln -sf "${PWD}/${file}" "${HOME}/${file}"
+    echo "Creating symlink ${HOME}/${file}"
+    ln -sf "${PWD}/${file}" "${HOME}/${file}"
 done
 
 for file in "${configs[@]}"; do
-	echo "Creating symlink ${HOME}/.config/${file}"
-	ln -sf "${PWD}/.config/${file}" "${HOME}/.config/${file}"
+    echo "Creating symlink ${HOME}/.config/${file}"
+    ln -sf "${PWD}/.config/${file}" "${HOME}/.config/${file}"
 done
