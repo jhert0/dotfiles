4b626583c4e31093b342b4164a9c96bcc9907a9b
endoffile78 <endoffile78@yahoo.com>
2020-03-14 18:02:25 -0500

    Update zsh prompt

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -6,32 +6,29 @@ setopt promptsubst
 
 virtualenv_info(){
     [[ -n ${VIRTUAL_ENV} ]] || return
-    echo "(%B%F{green}${VIRTUAL_ENV:t}%b) "
+    echo " %F{green}%B(venv:${VIRTUAL_ENV:t})%b%F{white}"
 }
 
 set_prompt(){
     PS1="%F{blue}%B%~%b%f"
 
-    local head=$(git rev-parse --verify HEAD 2>/dev/null)
-    if [[ $? -eq 0 ]]; then
-        if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
-            PS1+=" %{$fg_bold[magenta]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
-            modified=$(git status --short | grep " M " | wc -l)
-            if [ $modified -gt 0 ]; then
-                PS1+=" %{$fg_bold[green]%}~$modified%{$reset_color%}"
-            fi
-            untracked=$(git status --short | grep "??" | wc -l)
-            if [ $untracked -gt 0 ]; then
-                PS1+=" %{$fg_bold[yellow]%}+$untracked%{$reset_color%}"
-            fi
-            deleted=$(git status --short | grep " D " | wc -l)
-            if [ $deleted -gt 0 ]; then
-                PS1+=" %{$fg_bold[red]%}-$deleted%{$reset_color%}"
-            fi
+    if git rev-parse --is-inside-work-tree 2> /dev/null | grep -q 'true' ; then
+        PS1+=" %{$fg_bold[magenta]%}$(git rev-parse --abbrev-ref HEAD)%{$reset_color%}"
+        modified=$(git status --short | grep " M " | wc -l)
+        if [ $modified -gt 0 ]; then
+            PS1+=" %{$fg_bold[green]%}~$modified%{$reset_color%}"
         fi
-
+        untracked=$(git status --short | grep "??" | wc -l)
+        if [ $untracked -gt 0 ]; then
+            PS1+=" %{$fg_bold[yellow]%}+$untracked%{$reset_color%}"
+        fi
+        deleted=$(git status --short | grep " D " | wc -l)
+        if [ $deleted -gt 0 ]; then
+            PS1+=" %{$fg_bold[red]%}-$deleted%{$reset_color%}"
+        fi
     fi
-    PS1+=" ❯ "
+
+    PS1+="$(virtualenv_info) ❯ "
 }
 
 typeset -a precmd_functions
