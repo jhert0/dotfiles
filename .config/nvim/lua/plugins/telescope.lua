e2babf0bb8641f48b361d8419e4eee42b4e29eef
endoffile78 <endoffile78@yahoo.com>
2022-03-15 23:13:32 -0500

    [neovim] switched to ivy theme for telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,38 @@
 local telescope = require('telescope')
+local actions = require('telescope.actions')
 
 telescope.setup({
     defaults = {
         layouts_strategy = 'center',
+        mappings = {
+            i = {
+                ["<esc>"] = actions.close
+            }
+        }
     },
     pickers = {
         find_files = {
             previewer = false,
-            theme = "dropdown",
+            theme = "ivy",
         },
         buffers = {
             previewer = false,
-            theme = "dropdown",
+            theme = "ivy",
         },
         git_files = {
             previewer = false,
-            theme = "dropown"
+            theme = "ivy"
         },
         git_branches = {
             previewer = false,
-            theme = "dropdown",
+            theme = "ivy",
+        },
+        commands = {
+            previewer = false,
+            theme = "ivy"
         },
         command_history = {
-            theme = "dropdown",
+            theme = "ivy",
         }
     },
 })
