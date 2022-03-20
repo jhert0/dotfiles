7e9fa9bc22e40a013114a9f37be86a875e65db82
endoffile78 <endoffile78@yahoo.com>
2022-03-19 21:54:52 -0500

    [neovim] switched to packer

--- .config/nvim/init.vim
+++ .config/nvim/init.vim
@@ -1,65 +1,8 @@
-filetype off
-call plug#begin('~/.config/nvim/bundle')
-
-" general
-
-Plug 'tpope/vim-commentary'
-Plug 'editorconfig/editorconfig-vim'
-Plug 'ntpeters/vim-better-whitespace'
-Plug 'markonm/traces.vim'
-Plug 'machakann/vim-sandwich'
-Plug 'justinmk/vim-sneak'
-Plug 'vimlab/split-term.vim'
-Plug 'tpope/vim-rsi'
-Plug 'nvim-lua/plenary.nvim'
-Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}
-Plug 'windwp/nvim-autopairs'
-Plug 'lukas-reineke/indent-blankline.nvim'
-Plug 'danymat/neogen'
-
-Plug 'ibhagwan/fzf-lua', {'branch': 'main'}
-
-" theming
-
-Plug 'nvim-lualine/lualine.nvim'
-Plug 'kyazdani42/nvim-web-devicons'
-Plug 'EdenEast/nightfox.nvim'
-
-" LSP
-
-Plug 'neovim/nvim-lspconfig'
-Plug 'williamboman/nvim-lsp-installer'
-Plug 'lukas-reineke/lsp-format.nvim'
-Plug 'ray-x/lsp_signature.nvim'
-Plug 'folke/trouble.nvim'
-
-" autocompletion
-Plug 'hrsh7th/cmp-nvim-lsp'
-Plug 'hrsh7th/cmp-buffer'
-Plug 'hrsh7th/cmp-path'
-Plug 'hrsh7th/cmp-cmdline'
-Plug 'hrsh7th/nvim-cmp'
-
-" snippets
-
-Plug 'L3MON4D3/LuaSnip'
-Plug 'saadparwaiz1/cmp_luasnip'
-
-" git
-
-Plug 'lewis6991/gitsigns.nvim'
-Plug 'tpope/vim-fugitive'
-
-" fun
-
-Plug 'koron/nyancat-vim'
-
-call plug#end()
-
 filetype plugin indent on
 syntax on
 
 lua require("general")
+lua require("plugs")
 lua require("keybindings")
 lua require("ui")
 lua require("lsp")
---
+++ .config/nvim/lua/plugs.lua
@@ -0,0 +1,60 @@
+local packer = require('packer')
+
+return packer.startup(function()
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lua/plenary.nvim'
+
+    -- general
+    use 'tpope/vim-commentary'
+    use 'ntpeters/vim-better-whitespace'
+    use 'markonm/traces.vim'
+    use 'justinmk/vim-sneak'
+    use 'vimlab/split-term.vim'
+    use 'tpope/vim-rsi'
+    use 'editorconfig/editorconfig-vim'
+    use { 'ibhagwan/fzf-lua', branch = 'main' }
+
+    -- programming
+    use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+    use 'machakann/vim-sandwich'
+    use 'windwp/nvim-autopairs'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'danymat/neogen'
+
+    -- theming
+    use 'nvim-lualine/lualine.nvim'
+    use 'kyazdani42/nvim-web-devicons'
+    use 'EdenEast/nightfox.nvim'
+
+    -- LSP
+    use 'neovim/nvim-lspconfig'
+    use 'williamboman/nvim-lsp-installer'
+    use 'lukas-reineke/lsp-format.nvim'
+    use 'ray-x/lsp_signature.nvim'
+    use 'folke/trouble.nvim'
+
+    -- autocompletion
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+
+    -- snippets
+    use 'L3MON4D3/LuaSnip'
+    use 'saadparwaiz1/cmp_luasnip'
+
+    -- git
+    use 'lewis6991/gitsigns.nvim'
+    use 'tpope/vim-fugitive'
+
+    -- fun
+    use 'koron/nyancat-vim'
+end)
+
+--vim.cmd([[
+--    augroup packer_user_config
+--      autocmd!
+--      autocmd BufWritePost plugs.lua source <afile> | PackerCompile
+--    augroup end
+--]])
