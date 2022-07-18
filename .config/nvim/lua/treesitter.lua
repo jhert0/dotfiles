aa1611748bc0ac23013cb3f217b6c961d496ed09
endoffile78 <endoffile78@yahoo.com>
2022-07-17 20:56:40 -0500

    add neovim to treesitter install list

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "python", "go", "c_sharp", "lua" },
+  ensure_installed = { "c", "cpp", "python", "go", "c_sharp", "lua", "vim" },
   auto_install = true,
   highlight = {
     additional_vim_regex_highlighting = false,
