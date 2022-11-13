13e153d012953fdfe45acd9e4516d53e889a51fd
Jeremy Hertel <dev@jhert.com>
2022-11-13 00:20:57 -0600

    [emacs] improvements to latex config

--- .emacs.d/modules/langs/latex/config.el
+++ .emacs.d/modules/langs/latex/config.el
@@ -1,9 +1,13 @@
 ;;; langs/latex/config.el -*- lexical-binding: t; -*-
 
 (use-package tex
-  :straight auctex)
+  :straight auctex
+  :config
+  (setq LaTeX-item-indent 0)
 
-(add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
+  (add-hook 'LaTeX-mode-hook 'auto-fill-mode)
+  (add-hook 'LaTeX-mode-hook 'visual-line-mode))
 
 (add-hook 'tex-mode-hook 'visual-line-mode)
 (add-hook 'latex-mode-hook 'visual-line-mode)
