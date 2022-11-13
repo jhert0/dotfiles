a65d1fb394c4e956101c3b005d5dc51e184f3ca8
Jeremy Hertel <dev@jhert.com>
2022-11-12 20:27:54 -0600

    [emacs] only install eglot if using an older version of emacs

--- .emacs.d/modules/tools/eglot/config.el
+++ .emacs.d/modules/tools/eglot/config.el
@@ -1,15 +1,17 @@
-(use-package eglot
-  :ensure
-  :commands eglot-ensure
-  :init
-  (add-hook 'python-mode-hook 'eglot-ensure)
-  (add-hook 'c-mode-hook 'eglot-ensure)
-  (add-hook 'c++-mode-hook 'eglot-ensure)
-  (add-hook 'go-mode-hook 'eglot-ensure)
-  (add-hook 'rust-mode-hook 'eglot-ensure)
-  (add-hook 'csharp-mode-hook 'eglot-ensure)
-  (add-hook 'scala-mode-hook 'eglot-ensure)
-  :config
-  (add-to-list 'eglot-server-programs '(csharp-mode . ("csharp-ls")))
-  (add-to-list 'eglot-server-programs '(scala-mode . ("metals")))
-  (setq eglot-autoshutdown t))
+(when (version< emacs-version "29")
+  (straight-use-package 'eglot))
+
+(require 'eglot)
+
+(setq eglot-autoshutdown t)
+
+(add-hook 'python-mode-hook 'eglot-ensure)
+(add-hook 'c-mode-hook 'eglot-ensure)
+(add-hook 'c++-mode-hook 'eglot-ensure)
+(add-hook 'go-mode-hook 'eglot-ensure)
+(add-hook 'rust-mode-hook 'eglot-ensure)
+(add-hook 'csharp-mode-hook 'eglot-ensure)
+(add-hook 'scala-mode-hook 'eglot-ensure)
+
+(add-to-list 'eglot-server-programs '(csharp-mode . ("csharp-ls")))
+(add-to-list 'eglot-server-programs '(scala-mode . ("metals")))
