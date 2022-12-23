e47bc3a22f85d32382f00e5f39f2d4db7f3cf591
Jeremy Hertel <dev@jhert.com>
2022-12-23 11:42:47 -0600

    [neovim] switched to tokyo dark them

--- .config/nvim/lua/config/general.lua
+++ .config/nvim/lua/config/general.lua
@@ -29,47 +29,50 @@ vim.opt.splitbelow = true
 vim.env.NVIM_TUI_ENABLE_TRUE_COLOR = 1
 vim.env.NVIM_TUI_ENABLE_CURSOR_SHAPE = 1
 
-local nightfox = require('nightfox')
+vim.g.tokyodark_enable_italic = false
+vim.cmd("colorscheme tokyodark")
 
-nightfox.setup({
-    options = {
-        transparent = false,
-        styles = {
-            comments = "italic",
-            functions = "italic",
-            keywords = "NONE",
-            numbers = "NONE",
-            strings = "NONE",
-            types = "NONE",
-            variables = "NONE",
-        },
-    },
-    palletes = {
-        duskfox = {
-            bg0 = "#010101",
-            bg1 = "#000000",
-            bg3 = "#232323",
-            comment = "#c9136c",
-        },
-    },
-    specs = {
-        duskfox = {
-            syntax = {
-                number = "orange.bright",
-            },
-        },
-    },
-    groups = {
-        duskfox = {
-            gitcommitSummary = { fg = 'palette.white', link = '' },
-            MatchParen = { fg = "yellow" },
-            GitSignsChange = { fg = "yellow" },
-            LineNr = { fg = "fg2" },
-        }
-    },
-})
+-- local nightfox = require('nightfox')
 
-vim.cmd([[colorscheme duskfox]])
+-- nightfox.setup({
+--     options = {
+--         transparent = false,
+--         styles = {
+--             comments = "italic",
+--             functions = "italic",
+--             keywords = "NONE",
+--             numbers = "NONE",
+--             strings = "NONE",
+--             types = "NONE",
+--             variables = "NONE",
+--         },
+--     },
+--     palletes = {
+--         duskfox = {
+--             bg0 = "#010101",
+--             bg1 = "#000000",
+--             bg3 = "#232323",
+--             comment = "#c9136c",
+--         },
+--     },
+--     specs = {
+--         duskfox = {
+--             syntax = {
+--                 number = "orange.bright",
+--             },
+--         },
+--     },
+--     groups = {
+--         duskfox = {
+--             gitcommitSummary = { fg = 'palette.white', link = '' },
+--             MatchParen = { fg = "yellow" },
+--             GitSignsChange = { fg = "yellow" },
+--             LineNr = { fg = "fg2" },
+--         }
+--     },
+-- })
+
+-- vim.cmd([[colorscheme duskfox]])
 
 require('lualine').setup({
     options = {
--- .config/nvim/lua/config/plugins/init.lua
+++ .config/nvim/lua/config/plugins/init.lua
@@ -100,6 +100,7 @@ return packer.startup(function()
     use 'nvim-lualine/lualine.nvim'
     use 'kyazdani42/nvim-web-devicons'
     use 'EdenEast/nightfox.nvim'
+    use 'tiagovla/tokyodark.nvim'
 
     -- LSP
     use 'neovim/nvim-lspconfig'
