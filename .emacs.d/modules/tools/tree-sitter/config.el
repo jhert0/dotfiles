c1ebea3f073b6507441ff59c23f27edd6a77f6ab
Jeremy Hertel <dev@jhert.com>
2023-01-02 20:14:18 -0600

    [emacs] improve tree sitter configuration

--- .emacs.d/modules/tools/tree-sitter/config.el
+++ .emacs.d/modules/tools/tree-sitter/config.el
@@ -7,3 +7,17 @@
 
   (use-package tree-sitter-langs
     :ensure))
+
+(when (and (not (version< emacs-version "29"))
+           (treesit-available-p))
+  (use-package tree-sitter-module
+    :straight (tree-sitter-module
+               :type git :host github
+               :repo "casouri/tree-sitter-module"
+               :pre-build (("./batch.sh"))
+               :files ("dist/*.so" "dist/*.dll" "dist/*.dylib"))
+    :init
+    ;; Search for tree-sitter modules in this packages build directory.
+    (with-eval-after-load 'treesit
+      (add-to-list 'treesit-extra-load-path
+                   (straight--build-dir "tree-sitter-module")))))
