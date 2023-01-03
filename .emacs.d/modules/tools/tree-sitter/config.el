731c8c4c505fd028afc97f2b1a3f10bf18edc5e0
Jeremy Hertel <dev@jhert.com>
2023-01-02 20:20:06 -0600

    [emacs] use tree-sitter versions of certain major modes

--- .emacs.d/modules/tools/tree-sitter/config.el
+++ .emacs.d/modules/tools/tree-sitter/config.el
@@ -20,4 +20,9 @@
     ;; Search for tree-sitter modules in this packages build directory.
     (with-eval-after-load 'treesit
       (add-to-list 'treesit-extra-load-path
-                   (straight--build-dir "tree-sitter-module")))))
+                   (straight--build-dir "tree-sitter-module"))))
+
+  (add-hook 'c-mode-hook 'c-ts-mode)
+  (add-hook 'c++-mode-hook 'c++-mode)
+  (add-hook 'python-mode-hook 'python-ts-mode)
+  (add-hook 'go-mode-hook 'go-ts-mode))
