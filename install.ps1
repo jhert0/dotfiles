fbb7af0991d82357b755a2b909f9fa3cce5449c0
Jeremy Hertel <dev@jhert.com>
2023-05-26 20:09:59 -0500

    update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,9 +1,11 @@
-$home = ${env:USERPROFILE}
+$profile = ${env:USERPROFILE}
 
 winget install --id LLVM.LLVM
 winget install --id Git.Git
 winget install --id GNU.Emacs
+winget install --id Alacritty.Alacritty
+winget install --id Neovim.Neovim
 
-New-Item -ItemType SymbolicLink -Path "${home}/AppData/Local/nvim/" -Target "{$pwd}/.config/nvim"
-New-Item -ItemType SymbolicLink -Path "${home}/.emacs.d" -Target "${pwd}/.emacs.d"
-New-Item -ItemType SymbolicLink -Path "${home}/.doom.d" -Target "${pwd}/.doom.d"
+New-Item -ItemType SymbolicLink -Path "${profile}/AppData/Local/nvim/" -Target "{$pwd}/.config/nvim"
+New-Item -ItemType SymbolicLink -Path "${profile}/.emacs.d" -Target "${pwd}/.emacs.d"
+New-Item -ItemType SymbolicLink -Path "${profile}/.doom.d" -Target "${pwd}/.doom.d"
