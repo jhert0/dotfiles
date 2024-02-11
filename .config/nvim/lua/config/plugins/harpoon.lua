43bd6725bdf5a9762f572e2d511b90237a67f50f
Jeremy Hertel <dev@jhert.com>
2024-02-11 00:49:38 -0600

    [neovim] update harpoon keybindings

--- .config/nvim/lua/config/plugins/harpoon.lua
+++ .config/nvim/lua/config/plugins/harpoon.lua
@@ -7,15 +7,16 @@ return {
 
         harpoon.setup()
 
-        vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-        vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+        vim.keymap.set("n", "<leader>ho", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end)
 
         vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
         vim.keymap.set("n", "<C-k>", function() harpoon:list():select(4) end)
         vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
 
-        vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-        vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        vim.keymap.set("n", "<leader>hp", function() harpoon:list():prev() end)
+        vim.keymap.set("n", "<leader>hn", function() harpoon:list():next() end)
     end
 }
