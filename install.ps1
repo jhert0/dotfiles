04d5f675279cc0ba6166d88823aff894abd11df3
Jeremy Hertel <dev@jhert.com>
2024-02-20 17:32:17 -0600

    update install script for windows

--- install.ps1
+++ install.ps1
@@ -1,11 +1,9 @@
 $profile = ${env:USERPROFILE}
+$pwd = Get-Location
 
-winget install --id LLVM.LLVM
-winget install --id Git.Git
-winget install --id GNU.Emacs
-winget install --id Alacritty.Alacritty
-winget install --id Neovim.Neovim
+winget install Git.Git
+winget install GNU.Emacs
+winget install Neovim.Neovim
 
 New-Item -ItemType SymbolicLink -Path "${profile}/AppData/Local/nvim/" -Target "{$pwd}/.config/nvim"
 New-Item -ItemType SymbolicLink -Path "${profile}/.emacs.d" -Target "${pwd}/.emacs.d"
-New-Item -ItemType SymbolicLink -Path "${profile}/.doom.d" -Target "${pwd}/.doom.d"
