ad61892f69a91d8c0b8b7e5e2c60964c1e2774ed
Jeremy Hertel <dev@jhert.com>
2024-02-20 17:23:15 -0600

    [neovim] update config

--- .config/nvim/after/ftplugin/tex.lua
+++ .config/nvim/after/ftplugin/tex.lua
@@ -1,4 +1,9 @@
 vim.opt_local.spell = true
 vim.opt_local.wrap = true
 vim.opt_local.formatoptions:append('t')
+
+vim.opt_local.tabstop = 2
+vim.opt_local.softtabstop = 2
+vim.opt_local.shiftwidth = 2
+
 vim.opt_local.colorcolumn = "0"
