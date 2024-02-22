e0c87bf7fdb71636dfe64b0886e7fcdcf457505d
Jeremy Hertel <dev@jhert.com>
2024-02-21 22:13:43 -0600

    update install script for windows

--- install.ps1
+++ install.ps1
@@ -1,9 +1,11 @@
 $profile = ${env:USERPROFILE}
-$pwd = Get-Location
 
 winget install Git.Git
 winget install GNU.Emacs
 winget install Neovim.Neovim
 
-New-Item -ItemType SymbolicLink -Path "${profile}/AppData/Local/nvim/" -Target "{$pwd}/.config/nvim"
-New-Item -ItemType SymbolicLink -Path "${profile}/.emacs.d" -Target "${pwd}/.emacs.d"
+New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/nvim/" -Target "$pwd/.config/nvim"
+New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/.emacs.d" -Target "$pwd/.emacs.d"
+New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/wezterm" -Target "$pwd/.config/wezterm"
+New-Item -ItemType SymbolicLink -Path "$profile/.gitconfig" -Target "$pwd/.gitconfig"
+New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/Code" -Target "$pwd/.config/Code"
