396cfba9702cd37ba4a8fa7f64cfb14e2cce9132
Jeremy Hertel <dev@jhert.com>
2024-02-25 11:06:54 -0600

    improve install scripts

--- install.ps1
+++ install.ps1
@@ -8,4 +8,4 @@ New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/nvim/" -Target "$p
 New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/.emacs.d" -Target "$pwd/.emacs.d"
 New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/wezterm" -Target "$pwd/.config/wezterm"
 New-Item -ItemType SymbolicLink -Path "$profile/.gitconfig" -Target "$pwd/.gitconfig"
-New-Item -ItemType SymbolicLink -Path "$profile/AppData/Local/Code" -Target "$pwd/.config/Code"
+New-Item -ItemType SymbolicLink -Path "$profile/AppData/Roaming/Code" -Target "$pwd/.config/Code"
