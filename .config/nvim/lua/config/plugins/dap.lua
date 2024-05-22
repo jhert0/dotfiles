74a5df6581e860b50557bf4f8a0128034c56ef52
Jeremy Hertel <dev@jhert.com>
2024-05-21 21:38:08 -0500

    [neovim] update plugins and dap-ui config

--- .config/nvim/lua/config/plugins/dap.lua
+++ .config/nvim/lua/config/plugins/dap.lua
@@ -1,6 +1,7 @@
 return {
     {
         'mfussenegger/nvim-dap',
+        dependencies = { "nvim-neotest/nvim-nio" },
         config = function()
             local dap = require('dap')
 
@@ -18,21 +19,23 @@ return {
 
     {
         'rcarriga/nvim-dap-ui',
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
         config = function()
             local dap = require("dap")
             local dap_ui = require("dapui")
 
             dap_ui.setup()
 
-            dap.listeners.after.event_initialized["dapui_config"] = function()
+            dap.listeners.before.attach.dapui_config = function()
                 dap_ui.open()
             end
-
-            dap.listeners.before.event_terminated["dapui_config"] = function()
+            dap.listeners.before.launch.dapui_config = function()
+                dap_ui.open()
+            end
+            dap.listeners.before.event_terminated.dapui_config = function()
                 dap_ui.close()
             end
-
-            dap.listeners.before.event_exited["dapui_config"] = function()
+            dap.listeners.before.event_exited.dapui_config = function()
                 dap_ui.close()
             end
         end
